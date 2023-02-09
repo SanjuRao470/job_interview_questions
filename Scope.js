cb3b69523971054e7250b11ea907ed3ad9261986
SANJU RAO <sanjurao8072@gmail.com>
2023-02-09 16:37:58 +0530

    first code

--- Scope.js
+++ Scope.js
@@ -24,16 +24,27 @@
 
 //function getData() {
 
-    //  let rollNo=2; {console.log('######',rollNo)}
-//     let c = 10
-// if (c == 10) {
-// let a = "hi"
-// console.log(a)   ** can be console only  inside the block scope. example: inside the curly braces.
-// }
+//  let rollNo=2; {console.log('######',rollNo)}
+// let c = 10
+//if (c == 10) {
+//let a = "hi"
+// console.log(a)   ** can be console only inside the block scope. example: inside the curly braces.
+//}
 // console.log(a)  ** here shown ReferenceError: a is not defined.
-    
-    
-// }
 
 
-getData()
+//}
+//getData()
+
+//const 
+//Using const keyword you can not reassign the value nor you can redeclare the variable with const . 
+//If you do it will give you error .
+// must be assign a value while declaration.
+
+//example  const a=10; ** correct
+//const a;  ** wrong
+ //a = 10
+
+ //const
+//const b = "red"
+//b = "blue"       // give you error because can't be updated
