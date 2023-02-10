8d15d06967f37ef1bbd7c9eb42c02ca4e16b9ef7
SANJU RAO <sanjurao8072@gmail.com>
2023-02-10 18:25:57 +0530

    first scope

--- Scope.js
+++ Scope.js
@@ -55,3 +55,90 @@
  //const
 //const b = "red"
 //b = "blue"       // give you error because can't be updated
+
+
+//### HOISTING
+// App()
+//  function App(){
+
+//    console.log('@@@', number)  
+//  }
+//  var number;
+//   number=10;
+//   console.log(number)   
+
+//  Important points:  javascript only hoists declaration not initialization.
+// Execution of code is in sequential ways.
+// It gives only two Errors. Undefined and Reference error .
+//declaration of functions, variables or classes to the top of their scope, prior to execution of the code. 
+
+
+ 
+// function Doc(){
+
+//     // console.log('$$$$',a)
+// }
+// Doc()
+// a =5;                    case-1
+//  it will give the result 5
+
+
+
+// var a;
+// console.log(a)           case-2
+//  a =20;
+// here is also run and the result is undefined.
+
+
+
+//  console.log(a)
+// var a;                       case-3    
+// a =6;
+// but here gives reference error : a is not defined.
+// because have to write console.log(a) right after declaration like in case-2
+
+
+
+//  const a =3 ;      case-1
+//  console.log(a)
+ 
+// let a;
+//console.log(a)    case-2   Typescript error: cannot access 'a' before initialization
+// a= 5;
+
+
+// a = 6;
+//console.log(a)  case-3  it works!
+// let a;
+
+
+
+// function Doc(){
+
+    // console.log('$$$$',a)
+// }
+// a = 4;
+// var a; 
+// console.log(a)
+// Doc() 
+
+// var a;
+// a =4;           1- it works!
+// console.log(a)
+
+// var a;
+// console.log(a)  2-  initialized by undefined  
+// a = 4;
+
+// a = 4;
+// var a;           3- it will give result
+// console.log(a)
+
+// a = 4;
+// console.log(a)    4 -initialized by undefined  
+// var a;  
+         
+//  console.log(a)
+//  var a;          5- undefined
+//  a = 4;
+
