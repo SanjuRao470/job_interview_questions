7b8dedad628731fd0b1e75d837de7990eb421e13
KM Sanju <ksanju@kloudrac.com>
2023-10-19 11:38:41 +0530

    Your commit message here

---
+++ PractiseSet.js
@@ -0,0 +1,19 @@
+//Question 1
+
+// let user = [{
+//     fname:"piya",
+//     lname:"sharma"
+// }]
+
+// let user1 =[...user ,{age:30}]  
+//console.log(user1)  // [{fname:"piya",lname:"sharma"},{age:30}]
+
+// let user1= [...user]
+// user1.fname = 'anisha'
+//console.log(user1) [ { fname: 'piya', lname: 'sharma' }, fname: 'anisha' ]
+
+//  let key = "fname" 
+// console.log(user[key]) //piya
+
+//  let key = prompt ("fname") 
+// console.log(user[key]) //fname
