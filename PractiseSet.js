500381e1bf032aa0616e4523c570e5604c2e34cc
KM Sanju <ksanju@kloudrac.com>
2023-10-25 11:25:14 +0530

    fc

--- PractiseSet.js
+++ PractiseSet.js
@@ -1,6 +1,7 @@
 //##OBJECTS
 //Question 1
 
+
 // let user = [{
 //     fname:"piya",
 //     lname:"sharma"
@@ -53,6 +54,33 @@
 // console.log("DataRequired"[4].toLowerCase()) //r
 // console.log("management"[0].toUpperCase()) //M
 
+
+//EXAMPLE********************************
+//let str = "this is the best product in  the india"
+// console.log(str.length) //37
+// console.log(str.length) //38
+// console.log(str[5]) //i
+// console.log(str[7]) //whiteSpace
+// console.log(str[40]) //undefined
+
+// str1= 'Thi' + str[3]
+// console.log(str1)//This
+
+// position =0
+//  let finalStr =str.slice(0,position) + str1 +str.slice(position)
+//  console.log(finalStr)//Thisthis is the best product in  the india
+
+// console.log("this is the best product in  the india".toUpperCase())
+// console.log("this is the best product in  the india"[0].toUpperCase())
+//THIS IS THE BEST PRODUCT IN  THE INDIA
+//T
+// console.log("Mine"[0].toLowerCase())
+// console.log("Mine".toLowerCase())
+//m
+//mine
+
+
+
 //TOPIC
 //Searching for a substring
 //There are multiple ways to look for a substring within a string.
@@ -71,6 +99,41 @@
 // alert( "Widget".includes("id", 3) ); // false, from position 3 there is no "id"
 // alert( "Widget".startsWith("Wid") ); // true, "Widget" starts with "Wid"
 // alert( "Widget".endsWith("get") ); // true, "Widget" ends with "get"
+
+
+//EXAMPLES
+
+ // indexOf, includes, startsWith, endSWith,lastIndexOf
+//let str = "this is the best product in  the india"
+//  console.log(str.length) //38
+//  console.log(str.indexOf('is'))//2
+//  console.log(str.indexOf('am'))//-1
+//  console.log(str.indexOf('india'))//33
+  //console.log(str.includes("india"))//true
+  //   console.log(str.includes("usa"))//false
+// console.log(str.startsWith('this'))//true
+//  console.log(str.endsWith("in"))//false
+//   console.log(str.lastIndexOf("india"))//33
+ 
+
+
+//GET A SUBSTRING
+ // slice,  substring,  substr
+
+//  let str = "this is the best product in  the india"
+//  //slice not include end
+//  // in case of slice always start<end if it is not like that the swap the   parameter
+//  console.log(str.slice(3,8))//s is 
+//  console.log(str.slice(3))// s is the best product in  the india
+//  console.log(str.slice(6,3))// empty
+// console.log(str.slice(3,6)) //by swap s i
+
+// Q- ucFirst ="john"==John
+// let ucFirst ="john"
+// let newStr = ucFirst[0].toUpperCase() + ucFirst.slice(1)
+// console.log(newStr)//John
+
+
 
 
 //TOPIC
